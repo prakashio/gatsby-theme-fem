751e6f71893cc9ad0eab352e0cde6a8effa560b2
Prakash Kumar <prakash.kumar@active.ai>
2020-05-29 19:13:47 +0530

    feat(theme-dev): add Theme UI

--- packages/gatsby-theme-docs/src/components/layout.js
+++ packages/gatsby-theme-docs/src/components/layout.js
@@ -1,10 +1,16 @@
-import React from 'react';
+/** @jsx jsx */
+import { Fragment } from 'react';
+import { jsx } from 'theme-ui';
 
 const Layout = ({ children }) => (
-  <React.Fragment>
-    <header>gatsby-theme-docs</header>
-    <main>{children}</main>
-  </React.Fragment>
+  <Fragment>
+    <header
+      sx={{ bg: 'primary', color: 'background', fontFamily: 'heading', p: 3 }}
+    >
+      gatsby-theme-docs
+    </header>
+    <main sx={{ mx: 'auto', maxWidth: 650, width: '90vw' }}>{children}</main>
+  </Fragment>
 );
 
 export default Layout;
