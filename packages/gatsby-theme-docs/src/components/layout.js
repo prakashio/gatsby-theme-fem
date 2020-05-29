dc192e86d6f3c1cee33c226e22632a5a99d0d56a
Prakash Kumar <prakash.kumar@active.ai>
2020-05-29 18:56:33 +0530

    feat(theme-dev): create page resolver added

--- packages/gatsby-theme-docs/src/components/layout.js
+++ packages/gatsby-theme-docs/src/components/layout.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 
-const Layout = ({children}) => (
-    <React.Fragment>
-        <header>gatsby-theme-docs</header>
-        <main>{children</main>
-    </React.Fragment>
-)
+const Layout = ({ children }) => (
+  <React.Fragment>
+    <header>gatsby-theme-docs</header>
+    <main>{children}</main>
+  </React.Fragment>
+);
 
-export default Layout
+export default Layout;
---
+++ packages/gatsby-theme-docs/src/templates/docs-page-template.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import DocsPage from '../components/docs-page';
+
+export const query = graphql`
+  query($pageID: String!) {
+    docsPage(id: { eq: $pageID }) {
+      title
+      updated(fromNow: true)
+      body
+    }
+  }
+`;
+
+const DocsPageTemplate = ({ data }) => (
+  <DocsPage page={data.docsPage}></DocsPage>
+);
+
+export default DocsPageTemplate;
